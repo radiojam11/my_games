88f01d43a0739fb7e5b354ba21314dd998614202
Gabrio94 <gabriot1994@gmail.com>
2016-11-28 23:31:02 +0100

    Update wifi_spoof.sh

--- WiFi_spoof/wifi_spoof.sh
+++ WiFi_spoof/wifi_spoof.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 #depends on macchanger
 
-if [ $# -lt 4 -o $EUID -ne 0 ]; then 
-	echo "[!] Usage: "$0" iface_name wifi_name target_ip target_mac"
-	echo "[!] Need to be root ! Depends on macchanger ! "
-	exit 1
+if [ $# -lt 5 -o $EUID -ne 0 ]; then
+        echo "[!] Usage: "$0" iface_name wifi_name target_ip target_mac route_ip"
+        echo "[!] Need to be root ! "
+        exit 1
 fi
 
-mac=$4
 ip=$3
 ap=$2
 iface=$1
-
+mac=$4
+route_ip=$5
 echo "[+] lets spoof $mac,$ip"
 
 service network-manager stop
+service dnsmasq stop
+service wpasupplicant stop
+
 sleep 1
 
 ifconfig $iface down
@@ -23,7 +26,11 @@ macchanger $iface -m $mac
 iwconfig $iface essid $ap
 ifconfig $iface $ip
 
-echo '[+] tryin to ping google.com ...'
+ifconfig $iface up
+
+route add defautl gw $route_ip
+
+echo '[+] tryin to ping 8.8.8.8 ...'
 ping 8.8.8.8 -c 4 -w 3
-echo '[+] Bless..'
+echo '[!] Bless..'
 exit 0
